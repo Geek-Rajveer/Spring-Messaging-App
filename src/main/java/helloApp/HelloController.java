9e7cbe28ede5d7d4a635ca0d8274ed5883199f51
Geek-Rajveer <120301212+Geek-Rajveer@users.noreply.github.com>
2025-02-21 15:16:15 +0530

    [Rajveer] Added . uc1

--- src/main/java/helloApp/HelloController.java
+++ src/main/java/helloApp/HelloController.java
@@ -1,8 +1,16 @@
 package helloApp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
+
+    //Say Hello
+    @GetMapping("/hello")
+    public String uc1(){
+        return "Hello from BridgeLabz";
+    }
+
 }
