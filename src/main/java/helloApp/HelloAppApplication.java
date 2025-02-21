e49ff9ec355819f2ed2f253bc93a790c3dc28fa1
Geek-Rajveer <120301212+Geek-Rajveer@users.noreply.github.com>
2025-02-21 14:59:57 +0530

    initial commit

---
+++ src/main/java/helloApp/HelloAppApplication.java
@@ -0,0 +1,13 @@
+package helloApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/helloApp/HelloController.java
@@ -0,0 +1,8 @@
+package helloApp;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+}
